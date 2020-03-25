29f55d4e8bb302d5cf009e3ff686af004e4dc2a9
Prad Humna Raj Lamichhane <cas021911@coderacademy.edu.au>
2020-03-26 09:50:07 +1100

    Added functions to let the users see the currency code list.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WebRequest2
 {
@@ -9,7 +10,42 @@ namespace WebRequest2
             Console.WriteLine("Hello and Welcome.");
             Console.WriteLine("Here you can get information about the historical exchange rates between different currencies.");
             Console.WriteLine("The data is supplied by the European Central Bank.");
+            LatestExchangeRates();
 
+            
+
+
+
+        }
+
+        public static void LatestExchangeRates()
+        {
+            Console.WriteLine("Do you know the currency code for your base currency? Type 'Y' for yes and 'N' for no.");
+            string response = Console.ReadLine();
+
+            if (response.ToLower()=="n")
+            {
+                CurrencyCodes();
+            }
+
+            Console.WriteLine("Please enter the currency code of your base currency.");
+            string baseCurrency = Console.ReadLine();
+
+        }
+
+        public static void CurrencyCodes()
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+            string filename = "CurrencyCode.txt";
+            string fullFilePath = docPath + filename;
+
+            
+
+            string[] fileContentsByLine = File.ReadAllLines(fullFilePath);
+            foreach (string line in fileContentsByLine)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
