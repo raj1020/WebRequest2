35ac07f50c87b9ae9e2c9b6099be1a932557a042
Prad Humna Raj Lamichhane <cas021911@coderacademy.edu.au>
2020-03-25 22:44:12 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebRequest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome.");
+            Console.WriteLine("Here you can get information about the historical exchange rates between different currencies.");
+            Console.WriteLine("The data is supplied by the European Central Bank.");
+
+        }
+    }
+}
