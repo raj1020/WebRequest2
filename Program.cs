11762f365d60bcea3c4c14fa9bd64e22bc0224f5
Prad Humna Raj Lamichhane <cas021911@coderacademy.edu.au>
2020-03-29 12:58:45 +1100

    Added a dictionary to provide options to the users. Also added other messages to improve user experience.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace WebRequest2
@@ -8,8 +9,27 @@ namespace WebRequest2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello and Welcome.");
-            Console.WriteLine("Here you can get information about the historical exchange rates between different currencies.");
-            Console.WriteLine("The data is supplied by the European Central Bank.");
+            Console.WriteLine("Here you can get information about the historical exchange rates between different currencies (the data is supplied by the European Central Bank) and contribute by expressing your opinion about the future exchange rate movements. .");
+            Console.WriteLine("Please choose one of the option numbers below: ");
+
+            Dictionary<int, string> optionList = new Dictionary<int, string>()
+            {
+                { 1, "Get the latest foreign exchange rates." },
+                { 2, "Get the historical foreign exchange rates." },
+                { 3, "Get the latest exchange rates between any two currencies." },
+                { 4, "Get the historical exchange rates between any two currencies." }
+        };
+
+            foreach (KeyValuePair<int, string> kvp in optionList)
+            {
+               
+                Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+            }
+
+
+
+
+
             LatestExchangeRates();
 
             
@@ -31,6 +51,9 @@ namespace WebRequest2
             Console.WriteLine("Please enter the currency code of your base currency.");
             string baseCurrency = Console.ReadLine();
 
+            Console.WriteLine("Please select the option about the information you want to view.");
+
+
         }
 
         public static void CurrencyCodes()
@@ -40,6 +63,21 @@ namespace WebRequest2
             string fullFilePath = docPath + filename;
 
             
+
+            string[] fileContentsByLine = File.ReadAllLines(fullFilePath);
+            foreach (string line in fileContentsByLine)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public static void InformationOption()
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+            string filename = "InfoOpt.txt";
+            string fullFilePath = docPath + filename;
+
+
 
             string[] fileContentsByLine = File.ReadAllLines(fullFilePath);
             foreach (string line in fileContentsByLine)
